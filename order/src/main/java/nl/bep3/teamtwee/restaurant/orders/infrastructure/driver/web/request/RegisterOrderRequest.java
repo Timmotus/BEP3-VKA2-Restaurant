6ed8494da37051e0d9a9c26a41c971bed6e82203
Timmotus <tim.vandijk@student.hu.nl>
2021-12-22 18:36:45 +0100

    add httpclientconfig

--- order/src/main/java/nl/bep3/teamtwee/restaurant/orders/infrastructure/driver/web/request/RegisterOrderRequest.java
+++ order/src/main/java/nl/bep3/teamtwee/restaurant/orders/infrastructure/driver/web/request/RegisterOrderRequest.java
@@ -2,10 +2,13 @@ package nl.bep3.teamtwee.restaurant.orders.infrastructure.driver.web.request;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public class RegisterOrderRequest {
     // TODO: validation
     public String zipCode;
     public String street;
     public Long streetNumber;
+    @JsonAlias({ "items" })
     public Map<String, Long> itemCounts;
 }
