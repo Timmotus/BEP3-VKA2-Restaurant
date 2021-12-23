89525203f4b32f787348ae4e5b71f47fba532d7c
inazumark <inazumarkmail@gmail.com>
2021-12-23 10:24:50 +0100

    Additional Inventory code cleanup

--- inventory/src/main/java/nl/bep3/teamtwee/inventory/infrastructure/driven/messaging/RabbitMqEventPublisher.java
+++ inventory/src/main/java/nl/bep3/teamtwee/inventory/infrastructure/driven/messaging/RabbitMqEventPublisher.java
@@ -1,15 +1,13 @@
 package nl.bep3.teamtwee.inventory.infrastructure.driven.messaging;
 
+import lombok.AllArgsConstructor;
 import nl.bep3.teamtwee.inventory.core.domain.event.IngredientEvent;
 import nl.bep3.teamtwee.inventory.core.port.messaging.IngredientEventPublisher;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+@AllArgsConstructor
 public class RabbitMqEventPublisher implements IngredientEventPublisher {
     private final RabbitTemplate rabbitTemplate;
-
-    public RabbitMqEventPublisher(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
 
     public void publish(IngredientEvent event) {
         this.rabbitTemplate.convertAndSend("", event.getEventKey(), event);
